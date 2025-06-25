a4a072c83b1d34838c588b17595b1709788d5a79
fawzikhaild <fawzifarij.com>
2025-06-25 21:55:42 +0300

    Add django_url to requirements

--- testdb/settings.py
+++ testdb/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,16 +88,18 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER':'postgres',
-        'PASSWORD':'a',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'test',
+    #     'USER':'postgres',
+    #     'PASSWORD':'a',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+    # }
 }
 
 
